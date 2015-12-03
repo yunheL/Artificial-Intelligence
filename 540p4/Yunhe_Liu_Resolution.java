bfdb6ba253a8cd79fe7d920008c10a17ae72af9a
yunhe Liu <liu348@wisc.edu>
2015-12-03 15:46:38 -0600

    eliminate conditions 2/11

--- 540p4/Yunhe_Liu_Resolution.java
+++ 540p4/Yunhe_Liu_Resolution.java
@@ -9,7 +9,7 @@ public class Yunhe_Liu_Resolution extends DrawableTree
 		this.tree = tree; 
 		dirtyTree = true;
 	}
-	
+
 	/*
 	 * This helper method removes all the children of the
 	 * XML node passed in to the method
@@ -32,7 +32,7 @@ public class Yunhe_Liu_Resolution extends DrawableTree
 			i++;
 		}
 	}
-	
+
 	/*
 	 * This helper recursively traverse through the tree
 	 * and replace biconditions with two conditions
@@ -42,9 +42,9 @@ public class Yunhe_Liu_Resolution extends DrawableTree
 		//base case
 		if(curr == null)
 			return;
-		
+
 		XML[] childrenList = curr.getChildren();
-		
+
 		int i = 0;
 		while(i < childrenList.length)
 		{
@@ -53,10 +53,10 @@ public class Yunhe_Liu_Resolution extends DrawableTree
 			{
 				//TODO - remove debug code
 				//System.out.println("here");
-				
+
 				//keep traversing biconditional children list
 				traverseTreeRemoveBiconditions(childrenList[i]);
-				
+
 				//remove bicondition
 				//validation: make sure childrenList[i] has at least
 				//two children
@@ -66,25 +66,25 @@ public class Yunhe_Liu_Resolution extends DrawableTree
 							+ " count error");
 					return;
 				}
-				
+
 				//get the left and right logic of the bicondition
 				XML leftLogic = childrenList[i].getChild(0);
 				XML rightLogic = childrenList[i].getChild(1);
-				
+
 				//eliminate biconditoin connect by "and"
 				childrenList[i].setName("and");
-				
+
 				//remove all the children for reconstruction
 				removeAllChildren(childrenList[i]);
-				
+
 				//add two conditon children
 				childrenList[i].addChild("condition");
 				childrenList[i].addChild("condition");
-				
+
 				//add logic to the left condition
 				childrenList[i].getChild(0).addChild(leftLogic);
 				childrenList[i].getChild(0).addChild(rightLogic);
-				
+
 				//add logic to the right condition
 				childrenList[i].getChild(1).addChild(rightLogic);
 				childrenList[i].getChild(1).addChild(leftLogic);
@@ -97,40 +97,106 @@ public class Yunhe_Liu_Resolution extends DrawableTree
 			i++;
 		}
 	}
-	
+
+	/*
+	 * This helper recursively traverse through the tree
+	 * and replace conditions with corresponding logic
+	 */
+	public void traverseTreeRemoveConditions(XML curr)
+	{
+		//base case
+		if(curr == null)
+			return;
+
+		XML[] childrenList = curr.getChildren();
+
+		int i = 0;
+		while(i < childrenList.length)
+		{
+			//if current node is condition
+			if(childrenList[i].getName().equalsIgnoreCase("condition") )
+			{
+				//TODO - remove debug code
+				//System.out.println("here");
+
+				//keep traversing conditional children list
+				traverseTreeRemoveConditions(childrenList[i]);
+
+				//remove condition
+				//validation: make sure childrenList[i] has at least
+				//two children
+				if(childrenList[i].getChildCount() != 2)
+				{
+					System.out.println("remove condition children"
+							+ " count error");
+					return;
+				}
+
+				//get the left and right logic of the condition
+				XML leftLogic = childrenList[i].getChild(0);
+				XML rightLogic = childrenList[i].getChild(1);
+
+				//eliminate condition connect by "or"
+				childrenList[i].setName("or");
+
+				//remove all the children for reconstruction
+				removeAllChildren(childrenList[i]);
+
+				//add two children of "or"
+				childrenList[i].addChild("not");
+				childrenList[i].addChild(rightLogic);
+
+				//add left logic as the children of "not"
+				childrenList[i].getChild(0).addChild(leftLogic);
+			}
+			else{
+				//keep traversing if childrenList[i] is not bicondition
+				traverseTreeRemoveConditions(childrenList[i]);
+			}
+			//don't forget the increase counter!
+			i++;
+		}
+	}
+
 	public void eliminateBiconditions()
 	{
 		// TODO - Implement the first step in converting logic in tree to CNF:
 		// Replace all biconditions with truth preserving conjunctions of conditions.
-		
+
 		//TODO - remove debug code
 		//System.out.println("tree is " + tree);
-		
+
 		//call helper method to eliminate Biconditions
 		traverseTreeRemoveBiconditions(tree);
-		
+
 		//set dirtyTree flag for display
 		dirtyTree = true;
 	}	
-	
+
 	public void eliminateConditions()
 	{
 		// TODO - Implement the second step in converting logic in tree to CNF:
 		// Replace all conditions with truth preserving disjunctions.
-	}
 		
+		//call helper method to eliminate conditions
+		traverseTreeRemoveConditions(tree);
+
+		//set dirtyTree flag for display
+		dirtyTree = true;
+	}
+
 	public void moveNegationInwards()
 	{
 		// TODO - Implement the third step in converting logic in tree to CNF:
 		// Move negations in a truth preserving way to apply only to literals.
 	}
-		
+
 	public void distributeOrsOverAnds()
 	{
 		// TODO - Implement the fourth step in converting logic in tree to CNF:
 		// Move negations in a truth preserving way to apply only to literals.
 	}
-		
+
 	public void collapse()
 	{
 		// TODO - Clean up logic in tree in preparation for Resolution:
@@ -143,7 +209,7 @@ public class Yunhe_Liu_Resolution extends DrawableTree
 		// 3) Also remove any clauses that are always true (tautologies)
 		// from your tree to help speed up resolution.
 	}	
-	
+
 	public boolean applyResolution()
 	{
 		// TODO - Implement resolution on the logic in tree.  New resolvents
@@ -164,14 +230,14 @@ public class Yunhe_Liu_Resolution extends DrawableTree
 		// be resolved, then return null instead.
 		return null;
 	}	
-	
+
 	// REQUIRED HELPERS: may be helpful to implement these before collapse(), applyResolution(), and resolve()
 	// Some terminology reminders regarding the following methods:
 	// atom: a single named proposition with no children independent of whether it is negated
 	// literal: either an atom-node containing a name, or a not-node with that atom as a child
 	// clause: an or-node, all the children of which are literals
 	// set: an and-node, all the children of which are clauses (disjunctions)
-		
+
 	public boolean isLiteralNegated(XML literal) 
 	{ 
 		// TODO - Implement to return true when this literal is negated and false otherwise.
@@ -183,26 +249,26 @@ public class Yunhe_Liu_Resolution extends DrawableTree
 		// TODO - Implement to return the name of the atom in this literal as a string.
 		return "";
 	}	
-	
+
 	public boolean clauseContainsLiteral(XML clause, String atom, boolean isNegated)
 	{
 		// TODO - Implement to return true when the provided clause contains a literal
 		// with the atomic name and negation (isNegated).  Otherwise, return false.		
 		return false;
 	}
-	
+
 	public boolean setContainsClause(XML set, XML clause)
 	{
 		// TODO - Implement to return true when the set contains a clause with the
 		// same set of literals as the clause parameter.  Otherwise, return false.
 		return false;
 	}
-	
+
 	public boolean clauseIsTautology(XML clause)
 	{
 		// TODO - Implement to return true when this clause contains a literal
 		// along with the negated form of that same literal.  Otherwise, return false.
 		return false;
 	}	
-	
+
 }
