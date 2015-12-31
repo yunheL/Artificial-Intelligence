b987756a5f2f6b40cf275367e7aba6fade9d1211
yunhe Liu <liu348@wisc.edu>
2015-12-31 12:52:13 +0800

    wordPattern initial commit

---
+++ leetCodePractice/leetCodePractice/WordPattern.java
@@ -0,0 +1,16 @@
+package leetCodePractice;
+
+public class WordPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String abba = "cat dog dog cat";
+		System.out.println(wordPattern("abba", abba));
+	}
+	
+	public static boolean wordPattern(String pattern, String str) {
+		return true;
+        
+    }
+
+}
