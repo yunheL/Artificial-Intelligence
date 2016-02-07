59af478b3a89b8933c448be3d0b4851bdce570bf
yunhe Liu <liu348@wisc.edu>
2016-02-07 15:29:21 -0600

    UniqueWordAbbre

--- leetCodePractice/leetCodePractice/UniqueWordAbbre.java
+++ leetCodePractice/leetCodePractice/UniqueWordAbbre.java
@@ -4,7 +4,10 @@ public class UniqueWordAbbre {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] dictionary = new String[]{"dig"};
 		
+		ValidWordAbbr vwa = new ValidWordAbbr(dictionary);
+		System.out.println(vwa.isUnique("dog"));
 		
 
 	}
--- leetCodePractice/leetCodePractice/ValidWordAbbr.java
+++ leetCodePractice/leetCodePractice/ValidWordAbbr.java
@@ -1,25 +1,71 @@
 package leetCodePractice;
 
 public class ValidWordAbbr {
+
 	private String[] dictionary;
 
-    public ValidWordAbbr(String[] dictionary) {
-        this.dictionary = dictionary;
-    }
+	public ValidWordAbbr(String[] dictionary) {
+		this.dictionary = dictionary;
+	}
 
-    public boolean isUnique(String word) {
-    	if (word.length() > 2)
-    	{
-    		
-    	}
-    	
-        String first = word.substring(0);
-        String last = word.substring(word.length()-1);
-        int num = word.length()-2;
-        String abbr = first;
-        
-        
-        return true;
-    }
+	public boolean isUnique(String word) {
+		StringBuilder abbr = new StringBuilder();
+		//String abbrString = "";
+		String first = "";
+		String last = "";
+		int num = 0;
+		int i = 0;
+		int count = 0;
+
+		if(word.length() == 0)
+			return true;
+
+		if(dictionary == null)
+			return true;
+
+		if(dictionary.length == 1 && dictionary[0].equals(""))
+			return true;
+
+		if (word.length() == 1)
+		{
+			abbr.append(word);
+			first = word;
+			last = word;
+		}
+		else if (word.length() == 2)
+		{
+			abbr.append(word);
+			first = word.substring(0,1);
+			last = word.substring(1);
+			num = 0;
+		}
+		else{
+			abbr.append(word);
+			first = word.substring(0,1);
+			last = word.substring(word.length()-1);
+			num = word.length()-2;
+		}
+
+		//abbrString = abbr.toString();
+
+		for(i = 0; i < dictionary.length; i++)
+		{
+			if(!dictionary[i].equals(word))
+			{
+				if(first.equals(dictionary[i].substring(0,1)))
+				{
+					if(last.equals(dictionary[i].substring(dictionary[i].length()-1)))
+					{
+						if(word.length() == dictionary[i].length())
+						{
+							return false;
+						}
+					}
+				}
+			}
+
+		}
+		return true;
+	}
 
 }
