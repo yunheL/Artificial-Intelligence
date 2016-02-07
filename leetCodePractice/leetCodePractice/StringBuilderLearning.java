24b675441b88e171e630c40601e1333b62717d6c
yunhe Liu <liu348@wisc.edu>
2016-02-07 16:05:48 -0600

    StringBuilder

--- leetCodePractice/leetCodePractice/StringBuilderLearning.java
+++ leetCodePractice/leetCodePractice/StringBuilderLearning.java
@@ -17,14 +17,29 @@ public class StringBuilderLearning {
 		result = result.append(test2);
 		result = result.append(test3);
 		
-		
 		result1 = result1.append(test).append(test1).append(test2).append(test3);
 		
 		System.out.println(result);
-		System.out.println(result1);
+		System.out.println(result1.toString());
 		
 		System.out.println(test1.equals(test2));
 		System.out.println(result.toString().equals(result1.toString()));
+		
+		System.out.println(result.capacity());
+		System.out.println(result.charAt(2));
+		System.out.println(result.codePointAt(3)); //return ascii seq in decimal
+		
+		result.ensureCapacity(100);
+		System.out.println(result.capacity());
+		System.out.println(result.lastIndexOf(test2));
+		
+		result.trimToSize();
+		System.out.println(result.capacity());
+		
+		System.out.println(result1.capacity());
+		System.out.println(result1.length());
+		
+		System.out.println(result1.reverse());
 		
 	}
 
